068001fbe393d5c0a0aca26d575f3ba1921fbf3d
JheysonDev <JheysonDev@outlook.com>
2020-11-09 19:40:38 -0500

    Create add command

--- core/commands.rs
+++ core/commands.rs
@@ -1,2 +1,3 @@
+pub mod add;
 pub mod init;
 pub mod start;
--- core/main.rs
+++ core/main.rs
@@ -5,17 +5,20 @@ pub mod utils;
 use clap::App;
 
 fn main() {
-    let matches = App::new("Sflynlang")
-        .about("Multiparadigm and cross-platform programming language.")
+    let matches = App::new("slang")
+        .about("Programming language.")
         .version("0.1.0")
-        .author("Sflynlang <sflynlang@outlook.com>")
+        .author("Daniel Solarte <danielsolartech@hotmail.com>")
         .subcommand(commands::init::info())
         .subcommand(commands::start::info())
+        .subcommand(commands::add::info())
         .get_matches();
 
     if let Some(ref matches) = matches.subcommand_matches("init") {
         std::process::exit(commands::init::run(matches));
     } else if let Some(ref matches) = matches.subcommand_matches("start") {
         std::process::exit(commands::start::run(matches));
+    } else if let Some(ref matches) = matches.subcommand_matches("add") {
+        std::process::exit(commands::add::run(matches));
     }
 }
